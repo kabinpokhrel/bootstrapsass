e829f74da8d5d245cb5b4d4c841d26a5b86c4ee7
kabin pokhrel <kabin pokhrel>
2016-10-23 20:47:01 +0530

    Initial Sass Setup with Bootstrap

---
+++ _/javascripts/_bootstrap.js
@@ -0,0 +1,22 @@
+//@prepros-prepend bootstrap/jquery.js
+//@prepros-prepend bootstrap/affix.js
+//@prepros-prepend bootstrap/transition.js
+//@prepros-prepend bootstrap/tooltip.js
+//@prepros-prepend bootstrap/alert.js
+//@prepros-prepend bootstrap/button.js
+//@prepros-prepend bootstrap/carousel.js
+//@prepros-prepend bootstrap/collapse.js
+//@prepros-prepend bootstrap/dropdown.js
+//@prepros-prepend bootstrap/modal.js
+//@prepros-prepend bootstrap/popover.js
+//@prepros-prepend bootstrap/scrollspy.js
+//@prepros-prepend bootstrap/tab.js
+
+
+
+
+
+
+
+
+
